2d0ef1408cb666cbf67e20fb64c248aedb602818
heyyakash <akashsharma2002@gmail.com>
2022-11-23 03:30:37 +0530

    fixed logout from exercise

--- pages/excercise.js
+++ pages/excercise.js
@@ -1,9 +1,15 @@
 import { getSession } from 'next-auth/react'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 import Main from '../components/Excercise/Main'
 
 const excercise = ({data}) => {
+  const router = useRouter()
+  useEffect(()=>{
+    if(!data) router.push('/login')
+  },[])
   return (
+    data&&
       <Main session = {data?.user} />
 
   )
