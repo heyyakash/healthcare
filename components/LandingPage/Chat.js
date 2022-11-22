457bf2b3fbdc0bfc869259088c66311ae990894f
heyyakash <akashsharma2002@gmail.com>
2022-11-22 16:13:14 +0530

    fixed some errors again

--- components/LandingPage/Chat.js
+++ components/LandingPage/Chat.js
@@ -1,32 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-export class Chat extends Component{
-    componentDidMount(){
-       
-    (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"39dc82ad4ba4c7f26d86a0717c071ecb1","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
+export class Chat extends Component {
+  componentDidMount() {
+
+    (function (d, m) {
+      var kommunicateSettings =
+        { "appId": "39dc82ad4ba4c7f26d86a0717c071ecb1", "popupWidget": true, "automaticChatOpenOnNavigation": true };
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
     })(document, window.kommunicate || {});
 
 
-    }
+  }
 
-render()
-{
-const Chat = () => {
-  return (
-    <div>
-     
-    </div>
-   
-  );
-}
-}
+  render() {
+    return (
+      <div>
+
+      </div>
+
+    );
+
+  }
 }
 
 export default Chat;
